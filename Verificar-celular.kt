169b6e896bfbff33df99a504123323788a42495d
Fábio Henrique <faboestudante@gmail.com>
2026-05-13 10:38:28 -0300

    Add Verificar-celular.kt for user input collection

---
+++ Verificar-celular.kt
@@ -0,0 +1,34 @@
+fun main() {
+    print("Digite o UUID - ")
+    val uuid = readln()
+    
+    print("Digite o Username - ")
+    var username = readln()
+    
+    print("Digite o BatteryPct: ")
+    var batterypct = readln()
+    
+    print("Digite a AppVersion- ")
+    val appversion = readln()
+    
+    print("Digite o IsConnected - ")
+    val isconnected = readln()
+    
+    println("")
+    
+    println("UUID - $uuid")
+    
+    println("Username - $username")
+    
+    println("BatteryPct - $batterypct")
+    
+    println("AppVersion - $appversion")
+    
+    println("IsConnected - $isconnected")
+}
+
+
+    
+        
+}
+
